c3c7729b54db2ec8820e6780c8f1907b7e6ac701
stradivarius96 <matthew.pavan@gmail.com>
2021-12-10 20:27:54 +0000

    8 solved

--- 0008.js
+++ 0008.js
@@ -1,3 +1,17 @@
 const fs = require('fs')
-let arr = fs.readFileSync('0008-input.txt').toString().split("\n")
-console.log(arr)
+
+console.time('Run Time')
+
+let arr = fs.readFileSync('0008-input.txt').toString().split("").filter(a => a != '\n')
+
+let largest = 0
+for (let i = 0 ; i < arr.length - 13; i++){
+    if (arr.slice(i,i+13).reduce((pv, cv) => pv * cv, 1) > largest){
+        largest = arr.slice(i,i+13).reduce((pv, cv) => pv * cv, 1)
+    }
+}
+
+
+console.log("Answer: " + largest)
+
+console.timeEnd('Run Time')
