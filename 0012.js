aa67556f4c4b019dfba5d6a2844e23c6341ee383
Matthew Pavan <matthew.pavan@gmail.com>
2021-12-11 18:19:32 -0500

    formatting

--- 0012.js
+++ 0012.js
@@ -4,7 +4,7 @@ let divisors = 0
 let i = 1
 let tri = 0
 
-while (divisors <= 500){
+while (divisors <= 500) {
     tri += i
     i++
     divisors = countDivisors(tri)
@@ -12,18 +12,15 @@ while (divisors <= 500){
 
 console.log(tri)
 
-function countDivisors(n)
-{
+function countDivisors(n) {
     let count = 0
-    for(let i = 1; i <= Math.sqrt(n); i++)
-    {
-        if (n % i == 0)
-        {
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
             // If divisors are equal, count only one
             if (parseInt(n / i, 10) == i)
-                count++         
+                count++
             // Otherwise count both
-            else 
+            else
                 count += 2
         }
     }
--- 0013.js
+++ 0013.js
@@ -4,15 +4,6 @@ console.time('Run Time')
 
 let arr = fs.readFileSync('0013-input.txt').toString().split("\n").map(a => parseInt(a))
 
-console.log(arr)
-// let largest = 0
-// for (let i = 0 ; i < arr.length - 13; i++){
-//     if (arr.slice(i,i+13).reduce((pv, cv) => pv * cv, 1) > largest){
-//         largest = arr.slice(i,i+13).reduce((pv, cv) => pv * cv, 1)
-//     }
-// }
-
-
- console.log("Answer: " + arr.reduce((cv,pv) => cv+pv, 0).toString().replace(".",'').slice(0,10))
+console.log("Answer: " + arr.reduce((cv, pv) => cv + pv, 0).toString().replace(".", '').slice(0, 10))
 
 console.timeEnd('Run Time')
